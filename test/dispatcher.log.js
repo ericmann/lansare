e7e71d26cf4bba1b1abec13182de1e2a8db27416
Eric Mann <eric@eamann.com>
2015-09-25 16:30:33 -0700

    Fix editorconfig and tab indentions

--- test/dispatcher.log.js
+++ test/dispatcher.log.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var assert = require( 'assert' ),
-  LogFile_Dispatcher = require( '../lib/dispatcher.log' );
+	LogFile_Dispatcher = require( '../lib/dispatcher.log' );
 
 describe( 'logfile dispatcher', function() {
-  it( 'defines a notify method', function() {
-    var dispatcher = new LogFile_Dispatcher();
+	it( 'defines a notify method', function() {
+		var dispatcher = new LogFile_Dispatcher();
 
-    assert( dispatcher.notify );
-  } );
+		assert( dispatcher.notify );
+	} );
 } );
--- test/dispatcher.newrelic.js
+++ test/dispatcher.newrelic.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var assert = require( 'assert' ),
-  NewRelic_Dispatcher = require( '../lib/dispatcher.newrelic' );
+	NewRelic_Dispatcher = require( '../lib/dispatcher.newrelic' );
 
 describe( 'newrelic dispatcher', function() {
-  it( 'defines a notify method', function() {
-    var dispatcher = new NewRelic_Dispatcher();
+	it( 'defines a notify method', function() {
+		var dispatcher = new NewRelic_Dispatcher();
 
-    assert( dispatcher.notify );
-  } );
+		assert( dispatcher.notify );
+	} );
 } );
