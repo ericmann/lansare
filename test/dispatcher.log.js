68c86aff5866eb18910380c74ce9a8ab51a3608c
Eric Mann <eric@eamann.com>
2015-09-25 16:23:19 -0700

    Add minimal tests to ensure child dispatchers define the notify() method.

---
+++ test/dispatcher.log.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var assert = require( 'assert' ),
+  LogFile_Dispatcher = require( '../lib/dispatcher.log' );
+
+describe( 'logfile dispatcher', function() {
+  it( 'defines a notify method', function() {
+    var dispatcher = new LogFile_Dispatcher();
+
+    assert( dispatcher.notify );
+  } );
+} );
